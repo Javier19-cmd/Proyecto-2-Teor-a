a2f6793a7a2fb71695968fb08a2f5ee465b4e099
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-01 10:02:31 -0600

    agregando funciones
    Falta arreglar las últimas dos

--- Proyecto_2.py
+++ Proyecto_2.py
@@ -7,6 +7,9 @@ Proyecto 2: Funciones lambda.
 
 #Definiendo las funciones alpha y beta que se usarán para todas las operaciones.
 #Función alpha.
+from audioop import mul
+
+
 alpha = lambda x: x + 1
 
 #Función beta.
@@ -26,7 +29,23 @@ two = lambda f, x: f(f(f(x)))
 #Función 3.
 three = lambda f, x: f(f(f(f(x))))
 
+#Función sucessor.
+sucessor = lambda n, f, x: f(n(f, x))
+
+#Función add.
+add = lambda n, m, f, x: n(f, m(f, x))
+
+#Función multiply.
+multiply = lambda n, m, f, x: n(m(f), x)
+
+#Función exponent.
+#exponent = lambda n, m, f, x: m(n, f, x)
+
 print("Función 0: ", zero(alpha, -1))
 print("Función 1: ", one(alpha, -1))
 print("Función 2: ", two(alpha, -1))
 print("Función 3: ", three(alpha, -1))
+print("Función sucessor: ", sucessor(three, alpha, -1))
+print("Función add: ", add(two, three, alpha, -2))
+print("Función multiply: ", multiply(two, three, alpha, -2))
+#print("Función exponent: ", exponent(two, three, alpha, -2))
