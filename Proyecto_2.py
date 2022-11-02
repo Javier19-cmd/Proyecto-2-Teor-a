b35497ca196e12eaf7e29c58f88744b092944e68
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-01 20:59:38 -0600

    :v:

--- Proyecto_2.py
+++ Proyecto_2.py
@@ -4,6 +4,9 @@ Carnet: 20159
 Sección: 10
 Proyecto 2: Funciones lambda.
 """
+import sys
+
+sys.setrecursionlimit(100000)
 
 #Definiendo las funciones alpha y beta que se usarán para todas las operaciones.
 #Función alpha.
@@ -15,38 +18,56 @@ beta = lambda x: 2 * x
 #Definiendo las funciones de los números naturales.
 
 #Función 0.
-zero = lambda f, x: f(x)
+cero = lambda f, x: f(x)
 
 #Función 1.
-one = lambda f, x: f(f(x))
+uno = lambda f, x: f(x)
 
 #Función 2.
-two = lambda f, x: f(f(f(x)))
+dos = lambda f, x: f(f(x))
 
 #Función 3.
-three = lambda f, x: f(f(f(f(x))))
+tres = lambda f, x: f(f(f(x)))
 
 #Función 4.
-four = lambda f, x: f(f(f(f(f(x)))))
+cuatro = lambda f, x: f(f(f(f(x))))
 
-#Función sucessor.
-sucessor = lambda n, f, x: f(n(f, x))
+#Función sucesor.
+sucesor = lambda n, f, x: f(n(f, x))
 
-#Función add.
-add = lambda n, m, f, x: m(f, x) + n(f, x)
+#Función suma.
+suma = lambda n, m, f, x: n(f, m(f, x)) #m(f, x) + n(f, x)
 
-#Función multiply. n(m(f, x))
-multiply = lambda n, m, f, x: n(f, m(f, x))
+#Función multiplicar. n(m(f, x))
+mul = lambda n, m, f, x: n(m(f, x))
 
-#Función exponent.
-exponent = lambda n, m, f, x: m(n, f, x)
+#Función exponente.
+exponente = lambda n, m, f, x: 1 if m == 0 else mul(n, m, f, x) * exponente(n, m, f, x)
 
-print("Función 0: ", zero(alpha, -1))
-print("Función 1: ", one(alpha, -1))
-print("Función 2: ", two(alpha, -1))
-print("Función 3: ", three(alpha, -1))
-print("Función 4: ", four(alpha, -1))
-print("Función sucessor: ", sucessor(four, alpha, -1))
-print("Función add: ", add(two, three, alpha, -1))
-print("Función multiply: ", multiply(two, three, alpha, -1))
-#print("Función exponent: ", exponent(two, three, alpha, -2))
+print()
+print("-------------------Alpha-------------------")
+print()
+
+print("Función 0: ", cero(alpha, -1))
+print("Función 1: ", uno(alpha, 0))
+print("Función 2: ", dos(alpha, 0))
+print("Función 3: ", tres(alpha, 0))
+print("Función 4: ", cuatro(alpha, 0))
+print("Función sucesor: ", sucesor(dos, alpha, 0))
+print("Función suma: ", suma(dos, tres, alpha, 0))
+print("Función multiplicación: ", mul(dos, tres, alpha, 0))
+#print("Función exponente: ", exponente(dos, tres, alpha, -1))
+
+print()
+print("--------------------Beta-------------------")
+print()
+
+print("Función 0: ", cero(beta, -1))
+print("Función 1: ", uno(beta, -1))
+print("Función 2: ", dos(beta, -1))
+print("Función 3: ", tres(beta, -1))
+print("Función 4: ", cuatro(beta, -1))
+print("Función sucesor: ", sucesor(cuatro, beta, -1))
+print("Función suma: ", suma(cero, tres, beta, -1))
+print("Función multiplicación: ", mul(cero, tres, beta, -1))
+#print("Función exponente: ", exponente(dos, tres, beta, -1))
