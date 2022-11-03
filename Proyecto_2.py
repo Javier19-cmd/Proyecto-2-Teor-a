0c18b7742ed50546c57080574533f4ec950f8efe
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-03 12:36:11 -0600

    proyecto terminado

--- Proyecto_2.py
+++ Proyecto_2.py
@@ -3,10 +3,16 @@ Nombre: Javier Sebastián Valle Balsells
 Carnet: 20159
 Sección: 10
 Proyecto 2: Funciones lambda.
-"""
-import sys
 
-sys.setrecursionlimit(100000)
+Referencias: 
+
+1. Como imprimier el resultado de una función lambda: https://stackoverflow.com/questions/67552413/printing-of-a-function-or-lambda
+"""
+
+def get_name(f): #Función para poder ver el resultado que computa la función lambda.
+    try: name = [k for k,v in globals().items() if v==f][0]
+    except: return f
+    return '%s: %s' % (name, f)
 
 #Definiendo las funciones alpha y beta que se usarán para todas las operaciones.
 #Función alpha.
@@ -18,59 +24,67 @@ beta = lambda x: 2 * x
 #Definiendo las funciones de los números naturales.
 
 #Función 0.
-cero = lambda f, x: x
+cero = lambda f: lambda x: x
 
 #Función 1.
-uno = lambda f, x: f(x)
+uno = lambda f: lambda x: f(x)
 
 #Función 2.
-dos = lambda f, x: f(f(x))
+dos = lambda f: lambda x: f(f(x))
 
 #Función 3.
-tres = lambda f, x: f(f(f(x)))
+tres = lambda f: lambda x: f(f(f(x)))
 
 #Función 4.
-cuatro = lambda f, x: f(f(f(f(x))))
+cuatro = lambda f: lambda x: f(f(f(f(x))))
 
 #Función 5.
-cinco = lambda f, x: f(f(f(f(f(x)))))
+cinco = lambda f: lambda x: f(f(f(f(f(x)))))
 
 #Función sucesor.
-sucesor = lambda n, f, x: f(n(f, x))
+sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
 
 #Función suma.
-suma = lambda n, m, f, x: n(f, m(f, x)) #m(f, x) + n(f, x)
+suma = lambda m: lambda n: lambda f: lambda x: m(f)(n(f)(x))
 
 #Función multiplicar. n(m(f, x)). m se va a componer con sigo misma n veces.
-mul = lambda n, m, f, x:  n(m(f, x)) #n(f, x) * m(f, x)
+multiplicar = lambda m: lambda n: lambda f: lambda x: m(n(f))(x)
 
 #Función exponente.
-exponente = lambda n, m, f, x: 1 if m == 0 else mul(n, m, f, x) * exponente(n, m, f, x)
+exponente = lambda m: lambda n: lambda f: lambda x: n(m)(f)(x)
+
+N = get_name #Función para obtener el nombre de la función.
+# T = lambda x: lambda y: x #Función para obtener el primer elemento de una tupla.
+# N(T(2)(3))
+
+# print(N(T(2)(3)))
 
 print()
 print("-------------------Alpha-------------------")
 print()
 
-print("Función 0: ", cero(alpha, 0))
-print("Función 1: ", uno(alpha, 0))
-print("Función 2: ", dos(alpha, 0))
-print("Función 3: ", tres(alpha, 0))
-print("Función 4: ", cuatro(alpha, 0))
-print("Función sucesor: ", sucesor(cuatro, alpha, 0))
-print("Función suma: ", suma(uno, dos, alpha, 0))
-print("Función multiplicación: ", mul(uno, dos, alpha, 5))
-#print("Función exponente: ", exponente(dos, tres, alpha, -1))
+print("Función 0: ", N(cero(alpha)(0)))
+print("Función 1: ", N(uno(alpha)(0)))
+print("Función 2: ", N(dos(alpha)(0)))
+print("Función 3: ", N(tres(alpha)(0)))
+print("Función 4: ", N(cuatro(alpha)(0)))
+print("Función 5: ", N(cinco(alpha)(0)))
+print("Función sucesor: ", N(sucesor(uno)(alpha)(0)))
+print("Función suma: ", N(suma(dos)(dos)(alpha)(0)))
+print("Función multiplicar: ", N(multiplicar(dos)(tres)(alpha)(0)))
+print("Función exponente: ", N(exponente(cinco)(cuatro)(alpha)(0)))
 
 print()
 print("--------------------Beta-------------------")
 print()
 
-print("Función 0: ", cero(beta, -1))
-print("Función 1: ", uno(beta, -1))
-print("Función 2: ", dos(beta, -1))
-print("Función 3: ", tres(beta, -1))
-print("Función 4: ", cuatro(beta, -1))
-print("Función sucesor: ", sucesor(cuatro, beta, -1))
-print("Función suma: ", suma(cuatro, tres, beta, -1))
-print("Función multiplicación: ", mul(cuatro, cinco, beta, -1))
-#print("Función exponente: ", exponente(dos, tres, beta, -1))
+print("Función 0: ", N(cero(beta)(-1)))
+print("Función 1: ", N(uno(beta)(-1)))
+print("Función 2: ", N(dos(beta)(-1)))
+print("Función 3: ", N(tres(beta)(-1)))
+print("Función 4: ", N(cuatro(beta)(-1)))
+print("Función 5: ", N(cinco(beta)(-1)))
+print("Función sucesor: ", N(sucesor(uno)(beta)(-1)))
+print("Función suma: ", N(suma(dos)(dos)(beta)(-1)))
+print("Función multiplicar: ", N(multiplicar(dos)(tres)(beta)(-1)))
+print("Función exponente: ", N(exponente(cinco)(cuatro)(beta)(-1)))
