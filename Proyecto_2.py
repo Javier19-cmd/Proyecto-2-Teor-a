4ae225d13f40283ed44ddcaa74cbe92bc319731b
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-11-14 20:33:29 -0600

    mejoras

--- Proyecto_2.py
+++ Proyecto_2.py
@@ -3,16 +3,7 @@ Nombre: Javier Sebastián Valle Balsells
 Carnet: 20159
 Sección: 10
 Proyecto 2: Funciones lambda.
-
-Referencias: 
-
-1. Como imprimier el resultado de una función lambda: https://stackoverflow.com/questions/67552413/printing-of-a-function-or-lambda
 """
-
-def get_name(f): #Función para poder ver el resultado que computa la función lambda.
-    try: name = [k for k,v in globals().items() if v==f][0]
-    except: return f
-    return '%s: %s' % (name, f)
 
 #Definiendo las funciones alpha y beta que se usarán para todas las operaciones.
 #Función alpha.
@@ -59,38 +50,31 @@ multiplicar = lambda m: lambda n: lambda f: lambda x: m(n(f))(x)
 #Función exponente. n(m)(f)(x) = m^n(f)(x). 
 exponente = lambda m: lambda n: lambda f: lambda x: n(m)(f)(x)
 
-N = get_name #Función para obtener el nombre de la función.
-# T = lambda x: lambda y: x #Función para obtener el primer elemento de una tupla.
-# N(T(2)(3))
-
-# print(N(T(2)(3)))
-
 print()
 print("-------------------Alpha-------------------")
 print()
-
-print("Función 0: ", N(cero(alpha)(0)))
-print("Función 1: ", N(uno(alpha)(0)))
-print("Función 2: ", N(dos(alpha)(0)))
-print("Función 3: ", N(tres(alpha)(0)))
-print("Función 4: ", N(cuatro(alpha)(0)))
-print("Función 5: ", N(cinco(alpha)(0)))
-print("Función sucesor: ", N(sucesor(cinco)(alpha)(0)))
-print("Función suma: ", N(suma(dos)(cinco)(alpha)(0)))
-print("Función multiplicar: ", N(multiplicar(cuatro)(tres)(alpha)(0)))
-print("Función exponente: ", N(exponente(seis)(siete)(alpha)(0)))
+print("Función cero: ",cero(alpha)(0))
+print("Función uno: ",uno(alpha)(0))
+print("Función dos: ",dos(alpha)(0))
+print("Función tres: ",tres(alpha)(0))
+print("Función cuatro: ",cuatro(alpha)(0))
+print("Función cinco: ",cinco(alpha)(0))
+print("Función sucesor de cero: ",sucesor(cero)(alpha)(0))
+print("Función suma: ", suma(cuatro)(tres)(alpha)(0))
+print("Función multiplicar: ", multiplicar(cuatro)(tres)(alpha)(0))
+print("Función exponente: ", exponente(cuatro)(tres)(alpha)(0))
 
 print()
 print("--------------------Beta-------------------")
 print()
 
-print("Función 0: ", N(cero(beta)(-1)))
-print("Función 1: ", N(uno(beta)(-1)))
-print("Función 2: ", N(dos(beta)(-1)))
-print("Función 3: ", N(tres(beta)(-1)))
-print("Función 4: ", N(cuatro(beta)(-1)))
-print("Función 5: ", N(cinco(beta)(-1)))
-print("Función sucesor: ", N(sucesor(uno)(beta)(-1)))
-print("Función suma: ", N(suma(dos)(dos)(beta)(-1)))
-print("Función multiplicar: ", N(multiplicar(dos)(tres)(beta)(-1)))
-print("Función exponente: ", N(exponente(cinco)(cuatro)(beta)(-1)))
+print("Función 0: ", cero(beta)(-1))
+print("Función 1: ", uno(beta)(-1))
+print("Función 2: ", dos(beta)(-1))
+print("Función 3: ", tres(beta)(-1))
+print("Función 4: ", cuatro(beta)(-1))
+print("Función 5: ", cinco(beta)(-1))
+print("Función sucesor: ", sucesor(uno)(beta)(-1))
+print("Función suma: ", suma(dos)(dos)(beta)(-1))
+print("Función multiplicar: ", multiplicar(dos)(tres)(beta)(-1))
+print("Función exponente: ", exponente(cinco)(cuatro)(beta)(-1))
